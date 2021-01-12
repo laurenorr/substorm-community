90b0a383463a40da061a3fa7bf5ea4a13ef27103
laurenorr <73614882+laurenorr@users.noreply.github.com>
2021-01-12 20:34:05 +0000

    Update Plot_1_Communities_Individual_Substorm.m

--- Matlab_functions/Plot_1_Communities_Individual_Substorm.m
+++ Matlab_functions/Plot_1_Communities_Individual_Substorm.m
@@ -1,3 +1,18 @@
+%<Plot_1_Communities_Individual_Substorm.m Plots figure 1 from Orr.et.al 2021.>
+%    Copyright (C) <2021>  <Lauren Orr>
+
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 clear 
 load('normalized_times_2.mat')
 load('R/RSubstorms_v4_w128_t5.mat')
