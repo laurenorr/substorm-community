91d4f187dcbac665414a63b22c50077bd446d7d6
laurenorr <73614882+laurenorr@users.noreply.github.com>
2021-01-12 20:32:59 +0000

    Update Plot_3_Modularity_histogram.m

--- Matlab_functions/Plot_3_Modularity_histogram.m
+++ Matlab_functions/Plot_3_Modularity_histogram.m
@@ -1,3 +1,19 @@
+%<Plot_3_Modularity_histogram.m Plots figure 3 from Orr.et.al 2021.>
+%    Copyright (C) <2021>  <Lauren Orr>
+
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 load('normalized_times_2.mat')
 load('networki_v4_w128_t5.mat')
 load('Substom_list_extreme_quiet.mat')
