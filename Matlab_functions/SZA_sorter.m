4e5d12fddc1a91675cef9ecbd7782fda811e32f0
laurenorr <73614882+laurenorr@users.noreply.github.com>
2021-01-12 20:36:17 +0000

    Update SZA_sorter.m

--- Matlab_functions/SZA_sorter.m
+++ Matlab_functions/SZA_sorter.m
@@ -1,6 +1,21 @@
-%Read in a year of SZA data txt downloaded from superMAG. Write as a structure
-%with data matrix- no. stations x minutes in year
-%and names vector- no.stations
+%<SZA_sorter.m Read in a year of SZA data txt downloaded from superMAG.>
+%    Copyright (C) <2021>  <Lauren Orr>
+
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%    Read in a year of SZA data txt downloaded from superMAG. Write as a structure
+%    with data matrix- no. stations x minutes in year
+%    and names vector- no.stations
 Q=dlmread('20180116-09-34-supermag.txt','\t',73,5);
 fileID=fopen('20180116-09-34-supermag.txt');
 names=textscan(fileID,'%s %*[^\n]');
