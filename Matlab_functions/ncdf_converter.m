1a1cddc755ffa6e1ff89e098eb7a9d45286f9b56
laurenorr <73614882+laurenorr@users.noreply.github.com>
2021-01-12 20:42:06 +0000

    Update ncdf_converter.m

--- Matlab_functions/ncdf_converter.m
+++ Matlab_functions/ncdf_converter.m
@@ -1,3 +1,19 @@
+%<ncdf_converter.m Code to read ncdf files into a matlab structure.>
+%    Copyright (C) <2021>  <Lauren Orr>
+
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 %Code to read ncdf files into a matlab structure. Data is saved as year month day in separate year folders between 1996 to
 %2001. Each day contains a normalization matrix of what proportion of the network would be connected if the correlation
 %threshold was t where t=0:0.001:1. This matrix will later be used for a
